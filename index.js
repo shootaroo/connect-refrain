86d8b6382d8d633c8c152b821480d753fd1049a6
shootaroo <shotaro.tsubouchi@gmail.com>
2014-12-03 13:11:39 +0900

    Use pathname

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports = function (options) {
   var refrain = require('refrain')(options);
   return function (req, res, next) {
-    var file = refrain.find(req.url);
+    var file = refrain.find(req._parsedUrl ? req._parsedUrl.pathname : req.url);
     if (file) {
       refrain.render(file, null, function (err, output) {
         res.end(output);
