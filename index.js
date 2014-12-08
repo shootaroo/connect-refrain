59f8c90aa62a8012a63df47a2195e5f42df9d197
shootaroo <shotaro.tsubouchi@gmail.com>
2014-12-08 20:03:51 +0900

    Fix error handling

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ module.exports = function (options) {
   var refrain = require('refrain')(options);
   return function (req, res, next) {
     var file = refrain.find(req._parsedUrl ? req._parsedUrl.pathname : req.url);
-    if (file) {
-      refrain.render(file, null, function (err, output) {
-        res.end(output);
-      });
-    } else {
-      next();
-    }
+    if (!file) return next();
+    refrain.render(file, null, function (err, output) {
+      if (err) return next(err);
+      res.end(output);
+    });
   };
 };
